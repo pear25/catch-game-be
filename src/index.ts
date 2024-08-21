9dfdafffea9be195a2457238b30324c1b44d0827
Pierson Lim <piersonlimas12@gmail.com>
2024-08-21 15:20:29 +0800

    build: add config for hosted instance

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
-import { AppDataSource } from './data-source';
+import { AppDataSource } from "./data-source";
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const userRoutes = require('./user/userRoutes');
-const corsOptions = require('./config/cors');
-const errorHandler = require('./errorHandler');
+const userRoutes = require("./user/userRoutes");
+const corsOptions = require("./config/cors");
+const errorHandler = require("./errorHandler");
 
-const PORT = process.env.PORT || 3000;
+const LOCAL_PORT = process.env.PORT || 3000;
+const PROD_PORT = process.env.API_URL || "";
+const isProd = process.env.PROD === "true";
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors(corsOptions));
 
 async function main() {
   await AppDataSource.initialize()
-    .then(() => console.log('created user table'))
+    .then(() => console.log("created user table"))
     .catch((error) => console.log(error));
 
   app.use(errorHandler);
-  app.use('/user', userRoutes);
+  app.use("/user", userRoutes);
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  app.listen(isProd ? PROD_PORT : LOCAL_PORT, () => {
+    console.log(`Server is running on port ${isProd ? PROD_PORT : LOCAL_PORT}`);
   });
 }
 
